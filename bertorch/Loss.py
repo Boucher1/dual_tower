dac4db8842f4968dd5304d3fcb5e12b205a97b35
Adiao <55309347+Boucher1@users.noreply.github.com>
2024-02-26 15:07:59 +0800

    Add files via upload

---
+++ bertorch/Loss.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/10
+# @Author  : hehl
+# @Software: PyCharm
+# @File    : Loss.py
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
+
+        if self.reduction == 'mean':
+            return torch.mean(focal_loss)
+        elif self.reduction == 'sum':
+            return torch.sum(focal_loss)
+        else:
+            return focal_loss
+
---
+++ bertorch/model_hehl_all.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/23
+# @Author  : hehl
+# @Software: PyCharm
+# @File    : model_hehl_all.py
+# coding=utf-8
+import sys
+
+sys.path.append("/bertorch_main\\bertorch")
+from semantic_model import BaseModel
+import abc
+import time
+from loguru import logger
+import torch
+import torch.nn.functional as F
+import numpy as np
+from torch import nn
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error
+
+
+class Scaled_Dot_Product_Attention(nn.Module):
+    '''Scaled Dot-Product Attention '''
+
+    def __init__(self):
+        super(Scaled_Dot_Product_Attention, self).__init__()
+
+    def forward(self, Q, K, V, scale=None):
+        attention = torch.matmul(Q, K.permute(0, 2, 1))
+        if scale:
+            attention = attention * scale
+        # if mask:  # TODO change this
+        #     attention = attention.masked_fill_(mask == 0, -1e9)
+        attention = F.softmax(attention, dim=-1)
+        context = torch.matmul(attention, V)
+        return context
+
+
+class Multi_Head_Attention(nn.Module):
+    def __init__(self, dim_model, num_head, dropout=0.0):
+        super(Multi_Head_Attention, self).__init__()
+        self.num_head = num_head
+        assert dim_model % num_head == 0
+        self.dim_head = dim_model // self.num_head
+        self.fc_Q = nn.Linear(dim_model, num_head * self.dim_head)
+        self.fc_K = nn.Linear(dim_model, num_head * self.dim_head)
+        self.fc_V = nn.Linear(dim_model, num_head * self.dim_head)
+        self.attention = Scaled_Dot_Product_Attention()
+        self.fc = nn.Linear(num_head * self.dim_head, dim_model)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(dim_model)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        Q = self.fc_Q(x)
+        K = self.fc_K(x)
+        V = self.fc_V(x)
+        Q = Q.view(batch_size * self.num_head, -1, self.dim_head)
+        K = K.view(batch_size * self.num_head, -1, self.dim_head)
+        V = V.view(batch_size * self.num_head, -1, self.dim_head)
+        # if mask:  # TODO
+        #     mask = mask.repeat(self.num_head, 1, 1)  # TODO change this
+        scale = K.size(-1) ** -0.5
+        context = self.attention(Q, K, V, scale)
+
+        context = context.view(batch_size, -1, self.dim_head * self.num_head)
+        out = self.fc(context)
+        out = self.dropout(out)
+        out = out + x
+        out = self.layer_norm(out)
+        return out
+
+
+class Position_wise_Feed_Forward(nn.Module):
+    def __init__(self, dim_model, hidden, dropout=0.0):
+        super(Position_wise_Feed_Forward, self).__init__()
+        self.fc1 = nn.Linear(dim_model, hidden)
+        self.fc2 = nn.Linear(hidden, dim_model)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(dim_model)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = F.relu(out)
+        out = self.fc2(out)
+        out = self.dropout(out)
+        out = out + x
+        out = self.layer_norm(out)
+        return out
+
+
+class Encoder(nn.Module):
+    def __init__(self, dim_model, num_head, hidden, dropout):
+        super(Encoder, self).__init__()
+        self.attention = Multi_Head_Attention(dim_model, num_head, dropout)
+        self.feed_forward = Position_wise_Feed_Forward(dim_model, hidden, dropout)
+
+    def forward(self, x):
+        out = self.attention(x)
+
+        out = self.feed_forward(out)
+
+        return out
+
+
+class SentenceBERT(BaseModel):
+    def __init__(
+            self,
+            pretrained_model,
+            num_labels,
+            pooling_mode='mean',
+            concat_rep=True,
+            concat_diff=True,
+            concat_multiply=False,
+            output_emb_size=None,
+    ):
+        super().__init__(pretrained_model, output_emb_size)
+
+        self.concat_rep = concat_rep  # True
+        self.concat_diff = concat_diff  # True
+        self.concat_multiply = concat_multiply  # False
+        self.pooling_mode = pooling_mode  # Linear
+
+        dim_model = 768
+        num_head = 3
+        hidden = 3072
+        dropout = 0.5
+        self.selfAttenQ = Encoder(dim_model, num_head, hidden, dropout)
+        self.selfAttenG = Encoder(dim_model, num_head, hidden, dropout)
+
+        self.Q_linear = nn.Linear(768, 768)
+        self.G_linear = nn.Linear(768, 768)
+        self.relu_Q = nn.ReLU()
+        self.relu_G = nn.ReLU()
+        # self.dropout = nn.Dropout(0.2)
+
+        self.cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+
+    def forward(
+            self,
+            input_ids,
+            pair_input_ids,
+            attention_mask=None,
+            token_type_ids=None,
+            pair_attention_mask=None,
+            pair_token_type_ids=None,
+            labels=None
+    ):
+        emb = self.encode(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            pooling_mode=self.pooling_mode,
+            normalize_to_unit=False
+        )
+
+        pair_emb = self.encode(
+            pair_input_ids,
+            attention_mask=pair_attention_mask,
+            token_type_ids=pair_token_type_ids,
+            pooling_mode=self.pooling_mode,
+            normalize_to_unit=False
+        )
+
+        """
+            idea:
+                在embedding之后进行交互
+        """
+        # print(emb.shape)
+        # print("old emb shape", emb.shape)  # old emb shape torch.Size([32, 768])
+        emb = self.selfAttenQ(emb)
+        emb = torch.mean(emb, dim=0)  # 降维 [32, 32, 768]
+        emb = self.Q_linear(emb)  # emb shape torch.Size([32, 768])
+        emb = self.relu_Q(emb)
+
+        pair_emb = self.selfAttenG(pair_emb)
+        pair_emb = torch.mean(pair_emb, dim=0)
+        pair_emb = self.G_linear(pair_emb)
+        pair_emb = self.relu_G(pair_emb)
+
+        output = self.cos(emb, pair_emb)  # output shape torch.Size([32])
+        return output
+
+    def add_optimizer(self, optimizer):
+        self.optimizer = optimizer
+
+    def reduce_lr(self):
+        logger.info("Reducing LR ......")
+        for g in self.optimizer.param_groups:
+            g['lr'] = g['lr'] / 2
+
+    def add_loss_op(self, loss_op):
+        self.loss_op = loss_op
+
+    def run_epoch(self, train_dataloader, dev_dataloader, device, epoch, epochs):
+        train_losses = []
+        losses = []
+        self.train()
+
+        if (epoch == int(epochs / 3)) or (epoch == int(2 * epochs / 3)):
+            self.reduce_lr()
+        for step, batch in enumerate(train_dataloader, start=1):
+            tic_train = time.time()
+            self.optimizer.zero_grad()
+
+            input_ids, token_type_ids, attention_mask, pair_input_ids, pair_token_type_ids, pair_attention_mask, labels = batch
+            input_ids = input_ids.to(device)
+            token_type_ids = token_type_ids.to(device)
+            attention_mask = attention_mask.to(device)
+            pair_input_ids = pair_input_ids.to(device)
+            pair_token_type_ids = pair_token_type_ids.to(device)
+            pair_attention_mask = pair_attention_mask.to(device)
+            labels = labels.to(device)
+
+            y_pred = self.__call__(
+                input_ids,
+                pair_input_ids,
+                token_type_ids=token_type_ids,
+                attention_mask=attention_mask,
+                pair_token_type_ids=pair_token_type_ids,
+                pair_attention_mask=pair_attention_mask
+            )
+            loss = self.loss_op(y_pred, labels.float()).mean()
+            loss.backward()
+            losses.append(loss.data.cpu().numpy())
+            self.optimizer.step()
+
+            if step % 100 == 0:
+                avg_train_loss = np.mean(losses)
+                train_losses.append(avg_train_loss)
+                predicted = y_pred.cpu().data.numpy()
+                MSE = mean_squared_error(predicted, labels.cpu().data.numpy())
+                # print(predicted)
+                j = 0
+                # for i in predicted:
+                #     # 阈值 0.5
+                #     if (predicted[j] > 0.80):
+                #         predicted[j] = 1
+                #     elif (predicted[j] < 0.50):
+                #         predicted[j] = 0
+                #     else:
+                #         predicted[j] = 2
+                #     j = j + 1
+                #
+                # # 将变为三分类 计算acc f1
+                # acc = metrics.accuracy_score(predicted,
+                #                              np.where(labels.cpu().data.numpy() == 0.7, 2, labels.cpu().data.numpy()))
+                # f1 = metrics.f1_score(predicted,
+                #                       np.where(labels.cpu().data.numpy() == 0.7, 2, labels.cpu().data.numpy()),
+                #                       average="weighted")
+
+                time_diff = time.time() - tic_train
+
+                logger.info(
+                    " batch: %d, loss: %.5f,MSE: %.5f,speed: %.2f step/s"
+                    % (step, loss, MSE, 100 / time_diff))
+                tic_train = time.time()
+                losses = []
+        MSE = evaluate(self, dev_dataloader, device)
+        return device
+
+
+def evaluate( model,test_dataloader, device):
+    """
+    Evaluate model performance on a given dataset.
+    """
+    tic_train = time.time()
+    all_preds, pred = [], []
+    all_y, true = [], []
+    model.eval()
+    with torch.no_grad():
+        for batch in test_dataloader:
+            input_ids, token_type_ids, attention_mask, pair_input_ids, pair_token_type_ids, pair_attention_mask, labels = batch
+            input_ids = input_ids.to(device)
+            token_type_ids = token_type_ids.to(device)
+            attention_mask = attention_mask.to(device)
+            pair_input_ids = pair_input_ids.to(device)
+            pair_token_type_ids = pair_token_type_ids.to(device)
+            pair_attention_mask = pair_attention_mask.to(device)
+            labels = labels.to(device)
+
+            y_pred = model(
+                input_ids,
+                pair_input_ids,
+                token_type_ids=token_type_ids,
+                attention_mask=attention_mask,
+                pair_token_type_ids=pair_token_type_ids,
+                pair_attention_mask=pair_attention_mask
+            )
+            predicted = y_pred.cpu().data.numpy()
+            pred.extend(predicted)
+            true.extend(labels.cpu().data.numpy())
+
+    MSE = mean_squared_error(pred, true)
+
+
+    time_diff = time.time() - tic_train
+    logger.info(
+        "Test MSE:%.5f, speed: %.2f step/s"
+        % (MSE, 20 / time_diff))
+    # print("ACC: " + str(acc * 100))
+    # print("F1: " + str(f1 * 100))
+    model.train()
+    return MSE
